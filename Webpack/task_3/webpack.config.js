c5e1a2b91a2205b9349501f32c70d8bf65afa4a9
Dalington6 <d.tunyinko@alustudent.com>
2026-01-07 01:00:14 +0200

    chore(webpack): configure multi-entry webpack with dev server
    - Set up three entry points: header, body, footer
    - Configure webpack-dev-server on port 8564
    - Add HtmlWebpackPlugin for automatic HTML generation
    - Add CleanWebpackPlugin to clean build folder
    - Enable inline source maps for debugging
    - Configure code splitting with splitChunks optimization

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8564,
+    open: true,
+  },
+  devtool: 'inline-source-map',
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
