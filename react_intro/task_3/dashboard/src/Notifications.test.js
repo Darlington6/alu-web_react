fb47e36237dbe02f28b16d9269433900d8ec6f2e
Darlington6 <d.tunyinko@alustudent.com>
2026-01-12 04:25:28 +0200

    task 6: Create React tests

---
+++ react_intro/task_3/dashboard/src/Notifications.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+describe('Notifications component', () => {
+  test('Notifications renders without crashing', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Notifications renders three list items', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('li')).toHaveLength(3);
+  });
+
+  test('Notifications renders the text "Here is the list of notifications"', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
+  });
+});
