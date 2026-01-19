3b1b4e35464005d29b233029ee61973169c13fea
Darlington6 <d.tunyinko@alustudent.com>
2026-01-20 01:28:19 +0200

    fix(test): remove Node 16+ polyfills from setupTests for Node 12 compatibility
    Remove stream/web (ReadableStream, TransformStream) and worker_threads (MessageChannel, MessagePort) imports that don't exist in Node 12. Keep only TextEncoder/TextDecoder from util which are available in Node 12.22+.

--- React_component/task_0/dashboard/config/setupTests.js
+++ React_component/task_0/dashboard/config/setupTests.js
@@ -1,14 +1,8 @@
 const { TextEncoder, TextDecoder } = require('util');
-const { ReadableStream, TransformStream } = require('stream/web');
-const { MessageChannel, MessagePort } = require('worker_threads');
 require('regenerator-runtime/runtime');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-global.ReadableStream = ReadableStream;
-global.TransformStream = TransformStream;
-global.MessageChannel = MessageChannel;
-global.MessagePort = MessagePort;
 
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
