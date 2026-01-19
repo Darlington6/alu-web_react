8306b5020165187cb470de1021994f617b427f85
Darlington6 <d.tunyinko@alustudent.com>
2026-01-19 20:57:20 +0200

    fix: resolve Jest test environment issues with Node.js API polyfills
    Add jsdom test environment and polyfills for TextEncoder, TextDecoder, ReadableStream, and other Node.js APIs required by undici/cheerio dependencies. Downgrade cheerio to 1.0.0-rc.12 for enzyme compatibility. All 9 test suites now passing.

--- React_component/task_0/dashboard/config/setupTests.js
+++ React_component/task_0/dashboard/config/setupTests.js
@@ -1,5 +1,16 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'regenerator-runtime/runtime'
+const { TextEncoder, TextDecoder } = require('util');
+const { ReadableStream, TransformStream } = require('stream/web');
+const { MessageChannel, MessagePort } = require('worker_threads');
+require('regenerator-runtime/runtime');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+global.ReadableStream = ReadableStream;
+global.TransformStream = TransformStream;
+global.MessageChannel = MessageChannel;
+global.MessagePort = MessagePort;
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
