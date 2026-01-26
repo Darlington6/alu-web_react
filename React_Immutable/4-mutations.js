48b188bb36db60309436663a4452d184b4eee073
Darlington6 <d.tunyinko@alustudent.com>
2026-01-26 13:35:55 +0200

    Fix task 4: withMutations using whitelisted map parameter

--- React_Immutable/4-mutations.js
+++ React_Immutable/4-mutations.js
@@ -9,7 +9,6 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map.withMutations(function () {
-  // use arguments to avoid introducing new variable names
-  arguments[0].set('2', 'Benjamin').set('4', 'Oliver');
-});
+export const map2 = map.withMutations((map) =>
+  map.set('2', 'Benjamin').set('4', 'Oliver')
+);
