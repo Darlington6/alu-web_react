bbfd73e370347ac565dcd47f43c23480936c6d68
Darlington6 <Darlington6@users.noreply.github.com>
2026-02-09 10:56:07 +0200

    Add connectors project task 0

---
+++ react_redux_connectors_and_providers/README.md
@@ -0,0 +1,3 @@
+# React Redux Connectors and Providers
+
+Project tasks for React Redux connectors and providers.
---
+++ react_redux_connectors_and_providers/task_0/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux';
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+export const markAsAread = (index) => ({
+  type: MARK_AS_READ,
+  index
+});
+
+export const setNotificationFilter = (filter) => ({
+  type: SET_TYPE_FILTER,
+  filter
+});
+
+export const boundMarkAsAread = (dispatch) =>
+  bindActionCreators(markAsAread, dispatch);
+
+export const boundSetNotificationFilter = (dispatch) =>
+  bindActionCreators(setNotificationFilter, dispatch);
