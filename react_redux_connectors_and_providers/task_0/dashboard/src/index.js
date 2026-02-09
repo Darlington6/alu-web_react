efb3689808ed3f22c447b02dd779e08a1510948f
Darlington6 <Darlington6@users.noreply.github.com>
2026-02-09 11:00:10 +0200

    Add store and provider

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App/App';
 import { Notifications } from './Notifications/Notifications';
+import uiReducer from './reducers/uiReducer';
 
 const rootId = document.getElementById("root");
+const store = createStore(uiReducer);
 // isLoggedIn={true}
 ReactDOM.render(
-  <>
-      <App />
-    </>,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
     rootId
 );
 
 if (module.hot && process.env.NODE_ENV === "development") {
-  module.hot.accept("./App", () => {
-    const NextApp = require("./App").default;
+  module.hot.accept("./App/App", () => {
+    const NextApp = require("./App/App").default;
     ReactDOM.render(
       <React.StrictMode>
-        <App />
+        <Provider store={ store }>
+          <NextApp />
+        </Provider>
       </React.StrictMode>,
       rootId
     );
